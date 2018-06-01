4654bd3c18d444dcb62cec59e9265b4c58322637
Nakul Monga <thatnakka@users.noreply.github.com>
2018-05-31 20:26:54 -0400

    Update knife.rb

--- configs/knife.rb
+++ configs/knife.rb
@@ -1,8 +1,8 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "nmonga"
-client_key               "#{current_dir}/nmonga.pem"
+node_name                "<UNAME>"
+client_key               "#{current_dir}/PEM_FILE"
 validation_client_name   "prod-validator"
 validation_key           "#{current_dir}/prod-validator.pem"
 chef_server_url          "https://chef-server.lab/organizations/prod"
